2afd37b238372e8d428e76039e49e24ee7a43603
marcos <eptaccio@gmail.com>
2020-05-21 14:24:05 -0300

    start deck manipulation tests

--- tests/deck/deck.spec.js
+++ tests/deck/deck.spec.js
@@ -5,32 +5,44 @@ const DECK_SIZE = 52
 const removeDuplications = list => [...new Set(list)]
 
 describe('deck', () => {
-  it('should create a new deck with correct size', () => {
-    const deck = createDeck()
-    expect(deck.cards.length).toEqual(DECK_SIZE)
-  })
+  describe('deck creation', () => {
+    it('should create a new deck with correct size', () => {
+      const deck = createDeck()
+      expect(deck.cards.length).toEqual(DECK_SIZE)
+    })
 
-  it('should create a new deck with correct suits', () => {
-    const deck = createDeck()
+    it('should create a new deck with correct suits', () => {
+      const deck = createDeck()
 
-    const suitsOnTheDeck = deck.cards.map(
-      card => card.suit
-    )
-
-    const currentSuits = removeDuplications(suitsOnTheDeck)
-
-    expect(currentSuits).toMatchObject(DECK_DEFINITION.suits)
-  })
-
-  it('should create a new deck correct values', () => {
-    const deck = createDeck()
-
-    for (const suit of DECK_DEFINITION.suits) {
-      const currentCards = deck.cards.filter(
-        card => card.suit === suit
+      const suitsOnTheDeck = deck.cards.map(
+        card => card.suit
       )
 
-      expect(currentCards.length).toEqual(13)
-    }
+      const currentSuits = removeDuplications(suitsOnTheDeck)
+
+      expect(currentSuits).toMatchObject(DECK_DEFINITION.suits)
+    })
+
+    it('should create a new deck correct values', () => {
+      const deck = createDeck()
+
+      for (const suit of DECK_DEFINITION.suits) {
+        const currentCards = deck.cards.filter(
+          card => card.suit === suit
+        )
+
+        expect(currentCards.length).toEqual(13)
+      }
+    })
+  })
+
+  describe('deck manipulation', () => {
+    it('should shuffle deck', () => {
+      const currentDeck = {
+        cards: [
+
+        ]
+      }
+    })
   })
 })
