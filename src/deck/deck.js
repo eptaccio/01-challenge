8d078bc879a2c6fc0d9230ce13d62ccad4a6ef3f
marcos <eptaccio@gmail.com>
2020-05-21 02:18:02 -0300

    create the deck definition

--- src/deck/deck.js
+++ src/deck/deck.js
@@ -1,7 +1,19 @@
+const DECK_DEFINITION = {
+  suits: [
+    'SPADES', 'DIAMONDS', 'CLUBS', 'HEARTS'
+  ],
+  values: [
+    'A', '2', '3',
+    '4', '5', '6',
+    '7', '8', '9',
+    '10', 'J', 'Q', 'K'
+  ]
+}
 const createDeck = () => {
 
 }
 
 module.exports = {
-  createDeck
+  createDeck,
+  DECK_DEFINITION
 }
