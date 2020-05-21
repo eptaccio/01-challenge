ce1e057891e3d460eb3ae32e20f9ca098e499862
marcos <eptaccio@gmail.com>
2020-05-21 02:24:00 -0300

    create suits verification test

--- tests/deck/deck.spec.js
+++ tests/deck/deck.spec.js
@@ -1,7 +1,24 @@
-const { createDeck } = require('deck/deck')
+const { createDeck, DECK_DEFINITION } = require('deck/deck')
+
+const DECK_SIZE = 52
+
+const removeDuplications = list => [...new Set(list)]
 
 describe('deck', () => {
-  it('should create a new deck', () => {
+  it('should create a new deck with correct size', () => {
+    const deck = createDeck()
+    expect(deck.cards.length).toEqual(DECK_SIZE)
+  })
 
+  it('should create a new deck with correct suits', () => {
+    const deck = createDeck()
+
+    const suitsOnTheDeck = deck.cards.map(
+      card => card.suit
+    )
+
+    const currentSuits = removeDuplications(suitsOnTheDeck)
+
+    expect(currentSuits).toMatchObject(DECK_DEFINITION.suits)
   })
 })
