a5c136e7c300eba51efa5ef919d6ec35af1571c2
marcos <eptaccio@gmail.com>
2020-05-22 12:22:49 -0300

    add shuffe MIDDLE and BOTTOm tests

--- tests/deck/deck.spec.js
+++ tests/deck/deck.spec.js
@@ -138,6 +138,45 @@ describe('deck', () => {
           expect(firstCard).toMatchObject(newCard)
           expect(cards.length).toEqual(currentDeckLength + 1)
         })
+
+        it('shuffle on MIDDLE', () => {
+          const { cards } = shuffleOn({
+            newCard,
+            cards: currentDeck.cards,
+            position: SHUFFLE_POSITIONS.MIDDLE
+          })
+
+          const currentDeckLength = currentDeck.cards.length
+
+          const expectedCards = [
+            currentDeck.cards[0],
+            currentDeck.cards[1],
+            newCard,
+            currentDeck.cards[2],
+            currentDeck.cards[3]
+          ]
+
+          expect(cards).toMatchObject(expectedCards)
+          expect(cards.length).toEqual(currentDeckLength + 1)
+        })
+
+        it('shuffle on BOTTOM', () => {
+          const { cards } = shuffleOn({
+            newCard,
+            cards: currentDeck.cards,
+            position: SHUFFLE_POSITIONS.BOTTOM
+          })
+
+          const currentDeckLength = currentDeck.cards.length
+
+          const expectedCards = [
+            ...currentDeck.cards,
+            newCard
+          ]
+
+          expect(cards).toMatchObject(expectedCards)
+          expect(cards.length).toEqual(currentDeckLength + 1)
+        })
       })
     })
 
