7aee652ab0959e43bb8e4569a67836682c2d4006
marcos <eptaccio@gmail.com>
2020-05-22 18:16:22 -0300

    use deck routes

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const { config } = require('./config')
 const bodyParser = require('body-parser')
 const express = require('express')
+
 const db = require('./src/db')
+
+const { deckRoutes } = require('./src/deck/routes')
 
 db.connect()
 
 const app = express()
 
 app.use(bodyParser.json())
+
+app.use('/deck', deckRoutes)
 
 app.get('/status', (_req, res) => res.send('ok'))
 
