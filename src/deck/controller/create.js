21519f08196a98e8296e793d5cdf7a2933dffb21
marcos <eptaccio@gmail.com>
2020-05-22 18:29:15 -0300

    implement deck creation

--- src/deck/controller/create.js
+++ src/deck/controller/create.js
@@ -1,5 +1,29 @@
-const create = (req, res) => {
-  res.send('hello stranger')
+const { createDeck, saveDeck, shuffle } = require('../deck/')
+const { deckResponse, errorResponse } = require('../responses')
+const { loggerBuilder } = require('../../utils')
+
+const logger = loggerBuilder(__filename)
+
+const create = async (req, res) => {
+  try {
+    const { quantity = 1 } = req.body
+
+    const decks = []
+
+    for (let index = 0; index < quantity; index++) {
+      decks.push(shuffle(createDeck()))
+    }
+
+    const results = await Promise.all(decks.map(saveDeck))
+
+    res.send(results.map(deckResponse))
+  } catch (error) {
+    logger.error(error)
+
+    res.status(400).send(errorResponse({
+      message: 'unexpected error on deck creation :('
+    }))
+  }
 }
 
 module.exports = {
